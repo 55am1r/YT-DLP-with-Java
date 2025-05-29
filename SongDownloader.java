112ae503994e49e8df29ec9c4a2795c016d8a932
55am1r <shaiksameer515595@gmail.com>
2025-05-29 21:22:47 +0530

    Updated code to any error prevent & stable

--- SongDownloader.java
+++ SongDownloader.java
@@ -5,22 +5,22 @@ public class SongDownloader {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Song Downloader by PredatorFx!");
+        System.out.println("\n----->Welcome to the Song Downloader by PredatorFx!<------\n");
         while (true) {
-            System.out.println("Want to download a Song or Video?\n(Press A for Audio, V for Video): ");
+            System.out.print("Want to download a Song or Video?\n(Press A for Audio, V for Video): ");
             String choice = scanner.nextLine().trim().toUpperCase();
             if (choice.equals("A")) {
-                System.out.print("Enter the URL of the Song to download: ");
+                System.out.print("\nEnter the URL of the Song to download: ");
                 String userUrl = scanner.nextLine();
                 downloadAudio(userUrl);
                 break;
             } else if (choice.equals("V")) {
-                System.out.print("Enter the URL of the Video to download: ");
+                System.out.print("\nEnter the URL of the Video to download: ");
                 String userUrl = scanner.nextLine();
                 downloadVideo(userUrl);
                 break;
             } else {
-                System.out.println("Invalid choice. Please enter A for Audio or V for Video.");
+                System.out.println("\nInvalid choice. Please enter A for Audio or V for Video.");
             }
         }
     }
@@ -65,14 +65,10 @@ public class SongDownloader {
         String videoType = "";
         while (true) {
             System.out.println(
-                    "Which Quality do you want to download? \n (Look in Youtube Quality Settings, Select the top quality available for your video)");
-            System.out.println(
-                    "1. 144p\t2. 240p\t3. 360p\t4. 480p\t5. 720p\t6. 1080p\t7. 1440p\t8. 2160p (Your choice): ");
+                    "\nWhich Quality do you want to download?\n -->(Look in Youtube Quality Settings, Select the top quality available for your video)");
+            System.out.print(
+                    "1. 144p\t2. 240p\t3. 360p\t4. 480p\n5. 720p\t6. 1080p\t7. 1440p\t8. 2160p -->(Your choice): ");
             int qualityChoice = scanner.nextInt();
-            System.out.println(
-                    "In Which Video Type do you want to download? ");
-            System.out.println("1. MP4\t2. MKV\t3. WEBM\t4. MOV (Your choice [defaulted to MP4]): ");
-            int videoTypeChoice = scanner.nextInt();
             switch (qualityChoice) {
                 case 1:
                     format = "144";
@@ -100,7 +96,12 @@ public class SongDownloader {
                     break;
                 default:
                     System.out.println("Invalid choice. Defaulting to highest quality available.");
+                    continue;
             }
+            System.out.println(
+                    "\nIn Which Video Type do you want to download?");
+            System.out.print("1. MP4\t2. MKV -->(Your choice [defaulted to MP4]): ");
+            int videoTypeChoice = scanner.nextInt();
             switch (videoTypeChoice) {
                 case 1:
                     videoType = "mp4";
@@ -108,21 +109,12 @@ public class SongDownloader {
                 case 2:
                     videoType = "mkv";
                     break;
-                case 3:
-                    videoType = "webm";
-                    break;
-                case 4:
-                    videoType = "mov";
-                    break;
                 default:
                     System.out.println("Invalid choice. Defaulting to MP4.");
                     videoType = "mp4";
+
             }
-            if (format.isEmpty()) {
-                continue;
-            } else {
-                break;
-            }
+            break;
         }
         System.out.println("Downloading video...");
         // Replace with actual URL
